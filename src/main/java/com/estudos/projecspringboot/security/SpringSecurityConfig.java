42369abce3bb7af5b84dd2c5bbf0d92c4dde1055
C. Henrique <camilo.henri.ch@gmail.com>
2024-05-22 22:35:32 -0300

    fix: h2 connect web

--- src/main/java/com/estudos/projecspringboot/security/SpringSecurityConfig.java
+++ src/main/java/com/estudos/projecspringboot/security/SpringSecurityConfig.java
@@ -1,14 +1,18 @@
 package com.estudos.projecspringboot.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -34,4 +38,14 @@ public class SpringSecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		http.formLogin(withDefaults());
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+
+		return http.build();
+	}
 }
